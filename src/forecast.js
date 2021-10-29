ad29a54410062ab4f80a9f2e9bab0d3bd085ac68
yabenbean <yabincr1999@gmail.com>
2021-10-29 11:22:03 -0700

    ale update 2.0

--- src/forecast.js
+++ src/forecast.js
@@ -36,20 +36,20 @@ class Forecast extends React.Component {
         
         
         </div>
-
+          
             <video src={video} loop autoPlay muted width="600" height="600"className="col-sm-12"></video>
           <div className="center1 col-sm-12">
-          <img src="/legend2.JPG" alt="airdescription" className="size" />
+          <img src="/legend2.JPG" alt="Legend2" className="size" />
           <ForecastMap id={1} />
           
-          <div className="col-12 col-md-2">
+          
             <div
-              className="layerlist-content center1"
+              className="content-layerlist size2 "
               id="layerlist1"
               ref={this.listRef}
               
             ></div>
-          </div> 
+          
           
 
             
