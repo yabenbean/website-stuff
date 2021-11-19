b7ab6937f40051fb9b8124b70c7edcc5db085bae
yabenbean <yabincr1999@gmail.com>
2021-11-18 21:51:13 -0800

    ale update 3.0

--- src/Components/NavbarComp.js
+++ src/Components/NavbarComp.js
@@ -1,11 +1,8 @@
 import * as React from "react";
 import { Container, Navbar, Nav, Form, FormControl, Button, NavDropdown } from "react-bootstrap";
 import { setAirQuality } from "../redux/actions/spinner.actions";
+import Search from "../search.js";
 import ".././airquality.style.css";
-import { Component } from 'react';
-
-
-
 
 class NavbarComp extends React.Component {
   didAirQualityLoad = false;
@@ -35,6 +32,139 @@ class NavbarComp extends React.Component {
     };
   }
 
+  retrieveDataFromPostal(postalCode) {
+    const { setAirQuality } = this.props;
+    fetch(
+      this.PostcodeForecastUrl +
+        this.state.postalCode +
+        "&days=5&units=I&key=" +
+        this.key
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          postalCode: "",
+          //cityName: data.city_name,
+          //stateCode: data.state_code,
+
+          weatherTemp: [
+            data.data[0].temp,
+            data.data[1].temp,
+            data.data[2].temp,
+            data.data[3].temp,
+            data.data[4].temp,
+          ],
+          weatherMinTemp: [
+            data.data[0].min_temp,
+            data.data[1].min_temp,
+            data.data[2].min_temp,
+            data.data[3].min_temp,
+            data.data[4].min_temp,
+          ],
+          weatherMaxTemp: [
+            data.data[0].max_temp,
+            data.data[1].max_temp,
+            data.data[2].max_temp,
+            data.data[3].max_temp,
+            data.data[4].max_temp,
+          ],
+          weatherIcon: [
+            data.data[0].weather.icon,
+            data.data[1].weather.icon,
+            data.data[2].weather.icon,
+            data.data[3].weather.icon,
+            data.data[4].weather.icon,
+          ],
+          date: [
+            data.data[0].datetime,
+            data.data[1].datetime,
+            data.data[2].datetime,
+            data.data[3].datetime,
+            data.data[4].datetime,
+          ],
+        });
+      });
+
+    fetch(this.PostcodeAqiUrl + this.state.postalCode + "&key=" + this.key)
+      .then((response) => response.json())
+      .then((data) => {
+        this.didAirQualityLoad = true;
+        this.setState({
+          cityName: data.data[0].city_name,
+          stateCode: data.data[0].state_code,
+          aqiCode: data.data[0].aqi,
+        });
+      });
+  }
+
+  retrieveDataFromCity(cityName){
+    const { setAirQuality } = this.props;
+    fetch(
+      this.CityForecastUrl +
+        this.state.postalCode +
+        "&country=US"+
+        "&days=5&units=I&key=" +
+        this.key
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          postalCode: "",
+          //cityName: data.city_name,
+          //stateCode: data.state_code,
+
+          weatherTemp: [
+            data.data[0].temp,
+            data.data[1].temp,
+            data.data[2].temp,
+            data.data[3].temp,
+            data.data[4].temp,
+          ],
+          weatherMinTemp: [
+            data.data[0].min_temp,
+            data.data[1].min_temp,
+            data.data[2].min_temp,
+            data.data[3].min_temp,
+            data.data[4].min_temp,
+          ],
+          weatherMaxTemp: [
+            data.data[0].max_temp,
+            data.data[1].max_temp,
+            data.data[2].max_temp,
+            data.data[3].max_temp,
+            data.data[4].max_temp,
+          ],
+          weatherIcon: [
+            data.data[0].weather.icon,
+            data.data[1].weather.icon,
+            data.data[2].weather.icon,
+            data.data[3].weather.icon,
+            data.data[4].weather.icon,
+          ],
+          date: [
+            data.data[0].datetime,
+            data.data[1].datetime,
+            data.data[2].datetime,
+            data.data[3].datetime,
+            data.data[4].datetime,
+          ],
+        });
+      });
+
+    fetch(this.CityAqiUrl + this.state.postalCode + "&country=US" + "&key=" + this.key)
+      .then((response) => response.json())
+      .then((data) => {
+        this.didAirQualityLoad = true;
+        this.setState({
+          cityName: data.data[0].city_name,
+          stateCode: data.data[0].state_code,
+          aqiCode: data.data[0].aqi,
+        });
+      });
+  }
+  componentDidMount() {
+    this.retrieveDataFromPostal();
+  }
 
   handleSubmit = (event) => {
     event.preventDefault();
@@ -61,7 +191,6 @@ class NavbarComp extends React.Component {
       [name]: value,
     });
   };
-
   render() {
     return (
       <div>
@@ -83,20 +212,94 @@ class NavbarComp extends React.Component {
             </form>
 
             <div className="colorfont3 mt-3 p-5">
-              
+              <h2>Current Air Quality</h2>
               <div className="aq-d-location">
-                <h4>
+                <strong>
                   Location: {this.state.cityName}, {this.state.stateCode}
-                </h4>
+                </strong>
               </div>
-              <div className="aq-d-aqi textsize2 ">
-                Todays Air Quality Index: {this.state.aqiCode}                 
-
-
+              <div className="aq-d-aqi">
+                Air Quality Index: {this.state.aqiCode}
               </div>
               <div className="weather-search-results">
                 <div className="row" id="row">
                   <div className="col-md holder">
+                    <div>
+                      <h2> {this.state.date[0]}</h2>
+                    </div>
+
+
+                    {/* <img
+                      id="aq-weather-icon"
+                      src={
+                        "/weather-icons/" + this.state.weatherIcon[0] + ".png"
+                      }
+                      alt="weather-icon"
+                    />
+
+                    <div>Temp: {this.state.weatherTemp[0]}&#8457;</div>
+                    <div>Min Temp: {this.state.weatherMinTemp[0]}&#8457;</div>
+                    <div>Max Temp: {this.state.weatherMaxTemp[0]}&#8457;</div>
+                  </div>
+                  <div className="col-md holder">
+                    <div>
+                      <b> {this.state.date[1]}</b>
+                    </div>
+                    <img
+                      id="aq-weather-icon"
+                      src={
+                        "/weather-icons/" + this.state.weatherIcon[1] + ".png"
+                      }
+                      alt="weather-icon"
+                    />
+                    <div>Temp:{this.state.weatherTemp[1]}&#8457;</div>
+                    <div>Min Temp: {this.state.weatherMinTemp[1]}&#8457;</div>
+                    <div>Max Temp: {this.state.weatherMaxTemp[1]}&#8457;</div>
+                  </div>
+                  <div className="col-md holder">
+                    <div>
+                      <b> {this.state.date[2]}</b>
+                    </div>
+                    <img
+                      id="aq-weather-icon"
+                      src={
+                        "/weather-icons/" + this.state.weatherIcon[2] + ".png"
+                      }
+                      alt="weather-icon"
+                    />
+                    <div>Temp:{this.state.weatherTemp[2]}&#8457;</div>
+                    <div>Min Temp: {this.state.weatherMinTemp[2]}&#8457;</div>
+                    <div>Max Temp: {this.state.weatherMaxTemp[2]}&#8457;</div>
+                  </div>
+                  <div className="col-md holder">
+                    <div>
+                      <b> {this.state.date[3]}</b>
+                    </div>
+                    <img
+                      id="aq-weather-icon"
+                      src={
+                        "/weather-icons/" + this.state.weatherIcon[3] + ".png"
+                      }
+                      alt="weather-icon"
+                    />
+                    <div>Temp:{this.state.weatherTemp[3]}&#8457;</div>
+                    <div>Min Temp: {this.state.weatherMinTemp[3]}&#8457;</div>
+                    <div>Max Temp: {this.state.weatherMaxTemp[3]}&#8457;</div>
+                  </div>
+                  <div className="col-md holder">
+                    <div>
+                      <b> {this.state.date[4]}</b>
+                    </div>
+                    <img
+                      id="aq-weather-icon"
+                      src={
+                        "/weather-icons/" + this.state.weatherIcon[4] + ".png"
+                      }
+                      alt="weather-icon"
+                    />
+                    <div>Temp:{this.state.weatherTemp[4]}&#8457;</div> */}
+                    {/* <div>Min Temp: {this.state.weatherMinTemp[4]}&#8457;</div>
+                    <div>Max Temp: {this.state.weatherMaxTemp[4]}&#8457;</div> */}
                   </div>
                 </div>
               </div>
@@ -127,6 +330,5 @@ class NavbarComp extends React.Component {
     );
   }
 }
-
 
 export default NavbarComp;
