285dc1a8a1779fb03c506309464fd25d8a37dde8
yabenbean <yabincr1999@gmail.com>
2022-02-24 22:22:11 -0800

    new gauges & background

--- src/Components/dashboard/graphs/airqualitygauge.component.jsx
+++ src/Components/dashboard/graphs/airqualitygauge.component.jsx
@@ -8,14 +8,13 @@ class AirQualityGauge extends Component{
   didAirQualityLoad = false;
   constructor(props) {
     super(props);
-    this.PostcodeAQ ="https://api.weatherbit.io/v2.0/current/airquality?postal_code=";
-    this.CityAQ= 
-      "https://api.weatherbit.io/v2.0/current/airquality?city=";
-      this.key = "5023eb593a7c49f5b6a6a9e5184b38df";
+    this.PostcodeAQ ="https://api.weatherbit.io/v2.0/current?postal_code=";
+    this.CityAQ= "https://api.weatherbit.io/v2.0/current/airquality?city=";
+      this.key = "db5d97de2f5e423bb3dd7e130101a7dd";
       this.state = {
-        postalCode: "91732",
+        postalCode: "90006",
         cityName: null,
-        aqi: null,
+        aqiCode: null,
       };
     }
 
@@ -30,8 +29,8 @@ class AirQualityGauge extends Component{
           
      
           this.setState({
-            cityName: data.city_name,
-            aqi: data.data[0].aqi,
+            cityName: data.data[0].city_name,
+            aqiCode: data.data[0].aqi,
           });
         });
   
@@ -41,15 +40,16 @@ class AirQualityGauge extends Component{
       const { setAirQuality } = this.props;
    
   
-      fetch(this.CityAQ + this.state.cityName + "&key=" + this.key)
-        .then((response) => response.json())
-        .then((data) => {
-          this.didAirQualityLoad = true;
-          this.setState({
-            cityName: data.city_name,
-            aqi: data.data[0].aqi,
-          });
+      fetch(this.CityAqiUrl + this.state.postalCode + "&country=US" + "&key=" + this.key)
+      .then((response) => response.json())
+      .then((data) => {
+        this.didAirQualityLoad = true;
+        this.setState({
+          cityName: data.data[0].city_name,
+          stateCode: data.data[0].state_code,
+          aqiCode: data.data[0].aqi,
         });
+      });
   
      
     }
@@ -63,7 +63,7 @@ class AirQualityGauge extends Component{
         let parsed = parseInt(this.state.postalCode);
     
         if (isNaN(parsed)){
-          this.retrieveDataFromCity(this.state.cityName);
+          this.retrieveDataFromCity(this.state.postalCode);
         }else{
           try {
             this.retrieveDataFromPostal(this.state.postalCode);
@@ -73,11 +73,11 @@ class AirQualityGauge extends Component{
           }
         }
         
-        this.setState({ postalCode: "" });
+        this.setState({ postalCode:""});
       };
     
       handleChange = (event) => {
-        const { name, value }  = event.target;
+        const { name, value } = event.target;
         this.setState({
           [name]: value,
         });
@@ -164,13 +164,16 @@ class AirQualityGauge extends Component{
         }
       },
         "series": [{
-          "values": [this.state.aqi],
+          "values": [this.state.aqiCode],
           backgroundColor: 'black',
           "indicator": [10,0,0,0,0.5]
         }]
       }
       
-          return <ZingChart data={myConfig} />
+          return <ZingChart data={myConfig} 
+          />
+          
+          
     }
 
     
